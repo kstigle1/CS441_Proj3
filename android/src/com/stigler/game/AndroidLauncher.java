014818788fc148e0717560c4890523679fb23554
stigglesworth3 <Kyle@Stigler.com>
2021-03-01 11:21:15 -0500

    Initial commit

---
+++ android/src/com/stigler/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.stigler.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.stigler.game.CS441Proj3;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new CS441Proj3(), config);
+	}
+}
